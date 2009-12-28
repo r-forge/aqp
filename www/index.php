39c04a165d8aa240fc1eeb60af795e22e7da575f
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2009-12-28 21:33:54 +0000

    adding basic website content
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,9 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<img src="dend_with_profiles.png" border="0">
+
+
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
