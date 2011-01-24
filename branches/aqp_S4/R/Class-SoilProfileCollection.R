916b1f401b6743e9ac422ef64fbe56b6e8d75926
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-24 08:22:11 +0000

    major refactoring on the class definition
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/aqp_S4/R/Class-SoilProfileCollection.R
@@ -0,0 +1,22 @@
+setClass(
+  Class='SoilProfileCollection', 
+  representation=representation(
+    ids="character",
+    site="data.frame",
+    profiles='list'
+  ),
+  prototype=prototype( 
+    ids=as.character(NA),
+    site=data.frame(),
+    profiles=list(new("Profile"))
+  ),
+  validity=function(object) {
+    # number of ids and number of profiles must match
+    if (length(object@ids) != length(object@profiles))
+      stop("number of ids and number of profiles must match")
+    # number of ids and number of sites must match
+    if (length(object@ids) != nrow(object@site))
+      stop("# number of ids and number of sites must match")
+    return(TRUE)
+  }
+)
