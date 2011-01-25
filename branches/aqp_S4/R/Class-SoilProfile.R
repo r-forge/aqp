0b3a7dd29bdb9b04e281de4983760fb26010d92c
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-25 22:55:13 +0000

    I'm trying to split that class in two. The SoilProfile class is now a Profile class + a horizons slot (which gives data available in each horizon)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@166 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfile.R
+++ branches/aqp_S4/R/Class-SoilProfile.R
@@ -3,14 +3,14 @@ setClass(
   representation=representation(
     "Profile", # a Profile object gives depths, id, units
     horizons='data.frame', # the data for each horizon
-    site='data.frame' # the data related to the site
+#     site='data.frame' # the data related to the site
   ), 
   prototype=prototype(
     depths=matrix(ncol=2),
     id=as.character(NA),
     depth_units=as.character(NA),
     horizons=data.frame(),
-    site=data.frame()
+#     site=data.frame()
   ),
   validity=function(object) {
     # If there is horizon data available
@@ -19,9 +19,9 @@ setClass(
       if (nrow(object@horizons) != nrow(object@depths))
         stop("number of horizons and number of samples along the profile do not match")
     }
-     # By definition site_data has one value per profile
-    if (nrow(object@site) > 1)
-      stop("site data has to have one per value property.")
+#      # By definition site_data has one value per profile
+#     if (nrow(object@site) > 1)
+#       stop("site data has to have one per value property.")
 
     return(TRUE)     
   }
