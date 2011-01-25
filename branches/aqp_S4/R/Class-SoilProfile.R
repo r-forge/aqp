da6106a77181a7cf1afd594b2b91d766b240896c
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-25 22:18:21 +0000

    added modifications due to the changes in the Profile class design. code cleaning and adding comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@165 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfile.R
+++ branches/aqp_S4/R/Class-SoilProfile.R
@@ -1,20 +1,28 @@
 setClass(
   Class='SoilProfile', 
   representation=representation(
-    "Profile",
-    site='data.frame'
+    "Profile", # a Profile object gives depths, id, units
+    horizons='data.frame', # the data for each horizon
+    site='data.frame' # the data related to the site
   ), 
   prototype=prototype(
     depths=matrix(ncol=2),
-    horizons=data.frame(),
     id=as.character(NA),
     depth_units=as.character(NA),
+    horizons=data.frame(),
     site=data.frame()
   ),
   validity=function(object) {
+    # If there is horizon data available
+    if (length(object@horizons) > 0 ) {
+      # number of horizon and number of horizon data must match
+      if (nrow(object@horizons) != nrow(object@depths))
+        stop("number of horizons and number of samples along the profile do not match")
+    }
      # By definition site_data has one value per profile
     if (nrow(object@site) > 1)
       stop("site data has to have one per value property.")
+
     return(TRUE)     
   }
 )
