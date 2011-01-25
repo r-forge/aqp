14de3c250a64b4bc6b799ac4bab4f6ce12c0dfe0
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-25 23:00:06 +0000

    adaptations from the change in the SoilProfile class --This line, aand those below, will be ignored--
    M    Class-SoilProfileCollection.R
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@168 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfileCollection.R
+++ branches/aqp_S4/R/Class-SoilProfileCollection.R
@@ -8,7 +8,7 @@ setClass(
   prototype=prototype( 
     ids=as.character(NA),
     site=data.frame(),
-    profiles=list(new("Profile"))
+    profiles=list(new("SoilProfile"))
   ),
   validity=function(object) {
     # number of ids and number of profiles must match
@@ -17,6 +17,7 @@ setClass(
     # number of ids and number of sites must match
     if (length(object@ids) != nrow(object@site))
       stop("# number of ids and number of sites must match")
+
     return(TRUE)
   }
 )
