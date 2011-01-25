8641b967a4eae3b94e3807acdbe031ac91f0e1df
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-25 22:11:59 +0000

    added more validity tests added more in-line comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-Profile.R
+++ branches/aqp_S4/R/Class-Profile.R
@@ -19,9 +19,23 @@ setClass(
 #       if (nrow(object@horizons) != nrow(object@depths))
 #         stop("number of horizons and number of samples along the profile do not match")
 #     }
+
+    # testing that the depths are passesd using a 2-col matrix
+    if (ncol(object@depths) != 2)
+      stop("unconsistent number of depths columns")
+    # if the depths are not void (everything NA)
+    if (!all(is.na(object@depths))) {
+      # testing that the depths are consistently ordered
+      if (any((object@depths[, 2] - object@depths[, 1]) < 0))
+	stop("the depths matrix must be ordered with top depths first, and bottom depths second")
+    }
      # Number of user_id must equals one (one per profile)
     if (length(object@id) != 1)
       stop("profile id must be unique")
+    # units must be unique
+    if (length(object@depth_units) != 1)
+      stop("depths must be expressed using one unique unit.")
+
     return(TRUE)     
   }
 )
