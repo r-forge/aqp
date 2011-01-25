210f85406234ef44f99e863f930d6bbdc4cb5c13
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-25 22:01:01 +0000

    Change in the Profile class design: I removed the horizon slot. The reason for that is that I want to construct a very general, basic class. So as is, Profile is just storing the most basic description of a Profile: * an id * depths {top, bottom} * units in which those depths are expressed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-Profile.R
+++ branches/aqp_S4/R/Class-Profile.R
@@ -2,23 +2,23 @@ setClass(
   Class="Profile", 
   representation=representation(
     depths='matrix', # 2-col matrix with top and bottom horizon depths
-    horizons='data.frame', # the data for each horizon
+#     horizons='data.frame', # the data for each horizon
     id='character', # change it to profile_id ??
     depth_units='character'
   ), 
   prototype=prototype(
     depths=matrix(ncol=2),
-    horizons=data.frame(),
+#     horizons=data.frame(),
     id=as.character(NA),
     depth_units=as.character(NA)
   ),
   validity=function(object) {
-    # If there is horizon data available
-    if (length(object@horizons) > 0 ) {
-      # number of horizon and number of horizon data must match
-      if (nrow(object@horizons) != nrow(object@depths))
-        stop("number of horizons and number of samples along the profile do not match")
-    }
+#     # If there is horizon data available
+#     if (length(object@horizons) > 0 ) {
+#       # number of horizon and number of horizon data must match
+#       if (nrow(object@horizons) != nrow(object@depths))
+#         stop("number of horizons and number of samples along the profile do not match")
+#     }
      # Number of user_id must equals one (one per profile)
     if (length(object@id) != 1)
       stop("profile id must be unique")
