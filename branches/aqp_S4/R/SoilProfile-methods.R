3afc45f5ea27481b9f8c660acc3978c5f5e54119
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-26 01:15:18 +0000

    * first commit * creation of a SP object from scratch or from a *Profile object
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@172 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/aqp_S4/R/SoilProfile-methods.R
@@ -0,0 +1,33 @@
+## initializer
+
+#' Initialises a new Profile object.
+#' 
+#' Profile objects are storing very basic metadata about a soil profile: 
+#' depths, id, and the units in which depths are expressed.
+#'
+#' @param depths a two-columns matrix with the top and bottom depths of each horizon in the profile.
+#' @param id a unique identification of the profile
+#' @param depth_units the unit in which horizon depths are expressed
+#'
+"SoilProfile" <- function(depths=matrix(ncol=2), id=as.character(NA), depth_units="cm", horizons=data.frame()){
+  # if the first argument is a Profile object, it is used to construct the SoilProfile object
+  if (!is(depths, "Profile")) {
+    # creation of the Profile object 
+    depths <- Profile(depths=depths, id=id, depth_units=depth_units)
+  }
+  # creation of the object (includes a validity check)
+  new("SoilProfile", as(depths, "Profile"), horizons=horizons)
+}
+
+## accessors
+
+if (!isGeneric("horizons"))
+  setGeneric("horizons", function(object, ...)
+    standardGeneric("horizons"))
+
+setMethod("horizons", "SoilProfile",
+#' Retrieves the horizon information within a profile
+#'
+  function(object)
+    object@horizons
+)
