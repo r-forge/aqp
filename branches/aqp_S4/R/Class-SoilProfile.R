d912e2c3d903d9826a31c983c339833237005ac9
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-26 01:09:53 +0000

    code cleaning
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@171 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfile.R
+++ branches/aqp_S4/R/Class-SoilProfile.R
@@ -2,15 +2,13 @@ setClass(
   Class='SoilProfile', 
   representation=representation(
     "Profile", # a Profile object gives depths, id, units
-    horizons='data.frame', # the data for each horizon
-#     site='data.frame' # the data related to the site
+    horizons='data.frame' # the data for each horizon
   ), 
   prototype=prototype(
     depths=matrix(ncol=2),
     id=as.character(NA),
     depth_units=as.character(NA),
-    horizons=data.frame(),
-#     site=data.frame()
+    horizons=data.frame()
   ),
   validity=function(object) {
     # If there is horizon data available
@@ -19,9 +17,6 @@ setClass(
       if (nrow(object@horizons) != nrow(object@depths))
         stop("number of horizons and number of samples along the profile do not match")
     }
-#      # By definition site_data has one value per profile
-#     if (nrow(object@site) > 1)
-#       stop("site data has to have one per value property.")
 
     return(TRUE)     
   }
