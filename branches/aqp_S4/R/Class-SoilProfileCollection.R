636761a6ee70fee850d5ac6e7667fc24dda78965
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-26 02:22:47 +0000

    bug squashing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@178 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfileCollection.R
+++ branches/aqp_S4/R/Class-SoilProfileCollection.R
@@ -1,22 +1,24 @@
 setClass(
   Class='SoilProfileCollection', 
   representation=representation(
-    ids="character",
-    site="data.frame",
-    profiles='list'
+    profiles='list', # list of SoilProfiles (ie profiles depths + horizon data)
+    site="data.frame", # data about the sampling sites
+    ids="character" # list of the profiles ids
   ),
-  prototype=prototype( 
-    ids=as.character(NA),
+  prototype=prototype(
+    profiles=list(new("SoilProfile")),
     site=data.frame(),
-    profiles=list(new("SoilProfile"))
+    ids=as.character(NA)
   ),
   validity=function(object) {
     # number of ids and number of profiles must match
-    if (length(object@ids) != length(object@profiles))
+    if (length(which(!is.na(object@ids))) != length(object@profiles))
       stop("number of ids and number of profiles must match")
-    # number of ids and number of sites must match
-    if (length(object@ids) != nrow(object@site))
-      stop("# number of ids and number of sites must match")
+    # if there is some site data
+    if (length(object@site) > 0)
+      # number of ids and number of sites must match  
+      if (length(which(!is.na(object@ids))) != nrow(object@site))
+	stop("number of ids and number of sites must match")
 
     return(TRUE)
   }
