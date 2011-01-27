397f90728d83085e0fee7febcc69cd0880bd7590
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-27 01:42:27 +0000

    bug squashing in depth<- and introduction of site<- setter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@195 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/setters.R
+++ branches/aqp_S4/R/setters.R
@@ -1,3 +1,5 @@
+## depths<- setter method - to create AQP objects
+##
 if (!isGeneric('depths<-'))
   setGeneric('depths<-', function(object, value) 
     standardGeneric('depths<-'))
@@ -26,7 +28,7 @@ setReplaceMethod("depths", "data.frame",
 
 	res <- Profile(depths=depths, id=profiles_id) # assemble as a Profile object
 	if (nrow(horizon_data) > 0) # if theres data left, we create a SoilProfile object
-	  res <- SoilProfile(depths, horizons=horizon_data)	  
+	  res <- SoilProfile(res, horizons=horizon_data)	  
       }
 
       # otherwise, we have a collection -> SoilProfileCollection
@@ -36,7 +38,7 @@ setReplaceMethod("depths", "data.frame",
 # 	  stop('Please install the "plyr" package.')
 	  
 	  # nm contains names for profiles_ids, top, bottom
-	  SP_list <- dlply(.data=object, .variables=nm[1], .progress='text',
+	  SP_list <- dlply(.data=object, .variables=nm[1],
 		  .fun=function(profile_i) {
 		    # get current user_id
 		    profile_id_i <- unique(as.character(profile_i[, idx[1]]))
@@ -57,5 +59,101 @@ setReplaceMethod("depths", "data.frame",
       stop('invalid initialization for SoilProfile object')
 
     res
+  }
+)
+
+## site<- setter method - to initialize site data
+## in SoilProfileDataFrame and SoilProfileCollection objects
+##
+if (!isGeneric('site<-'))
+  setGeneric('site<-', function(object, value) 
+    standardGeneric('site<-'))
+
+setReplaceMethod("site", "Profile",
+  function(object, value) {
+    # cant do that on a Profile object as it does not have any data at all (excpet depths)
+    if (inherits(value, "formula") & (class(object) != "Profile")) {
+      mf <- model.frame(value, horizons(object))
+      idx <- match(names(mf), names(horizons(object)))
+
+      # assemble site_data, this is a 1-row data.frame
+      # since these data are repeated for each horizon, just keep the first
+      
+      # when there is only one attribute for site data we need to use a different approach
+      if(ncol(mf) < 2) {
+	site_data <- data.frame(X1=mf[1,])
+	names(site_data) <- names(mf)
+      }
+      # otherwise we just take the first row
+      else {
+	# checking unicity of the attributes along the profile
+	id_error <- which(sapply(mf, function(x){length(unique(x))}) > 1)
+	if (length(id_error) == 0)
+	  site_data <- mf[1, ]
+	else
+	  stop('site values are not site-specific')
+      }
+      
+      # remove the named site data from horizon_data
+      object@horizons <- horizons(object)[, -idx]
+      # update or create the site data
+      if (inherits(object, "SoilProfileDataFrame"))
+	object@site <- site_data # assign to object's slot
+      else # creation of a SoilProfileDataFrame object
+	object <- SoilProfileDataFrame(object, site=site_data)
+    }
+
+    else
+      stop('not implemented yet')
+  }
+)
+
+setReplaceMethod("site", "SoilProfileCollection",
+  function(object, value) {
+    if (inherits(value, "formula")) {
+      mf <- model.frame(value, horizons(object))
+      names_attr <- names(mf)
+      idx <- match(names(mf), names(horizons(object)))
+      mf <- data.frame(mf, 
+	PID=unlist(
+	  llply(profile_id(object), 
+	    function(x){
+	      rep(x, length.out=length(profiles(object, x)))
+	    })
+	, use.names=F))
+      # assemble site_data, this is a 1-row data.frame
+      # since these data are repeated for each horizon, just keep the first
+      
+      # when there is only one attribute for site data we need to use a different approach
+      site_data <- ddply(mf, .(PID), 
+	  .fun=function(x) {
+	    df <- subset(x, select=names_attr)
+	    colwise(unique)(df)
+	  })
+#       if(ncol(mf) < 2) {
+# 	site_data <- data.frame(X1=mf[1,])
+# 	names(site_data) <- names(mf)
+#       }
+#       # otherwise we just take the first row
+#       else {
+# 	# checking unicity of the attributes along the profile
+# 	id_error <- which(sapply(mf, function(x){length(unique(x))}) > 1)
+# 	if (length(id_error) == 0)
+# 	  site_data <- mf[1, ]
+# 	else
+# 	  stop('site values are not site-specific')
+#       }
+      
+      # update/create the site data slot
+      object@site <- site_data
+      # remove the named site data from horizon_data IN EACH PROFILE
+      l_ply(.data=profiles(object), .fun=function(x){
+	idx <- match(names_attr, names(horizons(x)))
+	x@horizons <- horizons(x)[, -idx]
+      })
+    }
+    else 
+      stop('not implemented yet')
+    object
   }
 )
