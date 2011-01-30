19618f6cae9309adf73e35e79708211a611195c4
roudierp (Dr. ) <pierre.roudier@gmail.com>
2011-01-30 01:23:23 +0000

    moved Profile vlidity checks here as I merge the Profile class with the SoilProfile class (for simplicity reasons)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfile.R
+++ branches/aqp_S4/R/Class-SoilProfile.R
@@ -1,7 +1,9 @@
 setClass(
   Class='SoilProfile', 
   representation=representation(
-    "Profile", # a Profile object gives depths, id, units
+    depths='matrix', # 2-col matrix with top and bottom horizon depths
+    id='character', # change it to profile_id ??
+    depth_units='character',
     horizons='data.frame' # the data for each horizon
   ), 
   prototype=prototype(
@@ -11,13 +13,27 @@ setClass(
     horizons=data.frame()
   ),
   validity=function(object) {
+    # testing that the depths are passesd using a 2-col matrix
+    if (ncol(object@depths) != 2)
+      stop("unconsistent number of depths columns")
+    # if the depths are not void (everything NA)
+    if (!all(is.na(object@depths))) {
+      # testing that the depths are consistently ordered
+      if (any((object@depths[, 2] - object@depths[, 1]) < 0))
+	stop("the depths matrix must be ordered with top depths first, and bottom depths second")
+    }
+     # Number of user_id must equals one (one per profile)
+    if (length(object@id) != 1)
+      stop("profile id must be unique")
+    # units must be unique
+    if (length(object@depth_units) != 1)
+      stop("depths must be expressed using one unique unit.")
     # If there is horizon data available
     if (length(object@horizons) > 0 ) {
       # number of horizon and number of horizon data must match
       if (nrow(object@horizons) != nrow(object@depths))
         stop("number of horizons and number of samples along the profile do not match")
     }
-
     return(TRUE)     
   }
 )
