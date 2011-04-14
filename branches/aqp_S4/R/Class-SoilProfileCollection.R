d1e9618d89b2f027d93d955734c15e2adf72fcd0
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2011-04-14 04:51:20 +0000

    fixing test suite, and working on site data assignment via external DF
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@250 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/aqp_S4/R/Class-SoilProfileCollection.R
+++ branches/aqp_S4/R/Class-SoilProfileCollection.R
@@ -3,23 +3,26 @@ setClass(
   representation=representation(
     profiles='list', # list of SoilProfiles (ie profiles depths + horizon data)
     site="data.frame", # data about the sampling sites
-    ids="character" # list of the profiles ids
+    ids="character", # list of the profiles ids
+    site_id="character" # name of column in site data that contains an id matching horizon data
   ),
   prototype=prototype(
     profiles=list(new("SoilProfile")),
     site=data.frame(),
-    ids=as.character(NA)
+    ids=as.character(NA),
+    site_id=as.character(NA)
   ),
   validity=function(object) {
     # number of ids and number of profiles must match
     if (length(which(!is.na(object@ids))) != length(object@profiles))
       stop("number of ids and number of profiles must match")
     # if there is some site data
-    if (length(object@site) > 0)
+    if (length(object@site) > 0) {
       # number of ids and number of sites must match  
       if (length(which(!is.na(object@ids))) != nrow(object@site))
-	stop("number of ids and number of sites must match")
-
+		 stop("number of ids and number of sites must match")
+	}
+    
     return(TRUE)
   }
 )
