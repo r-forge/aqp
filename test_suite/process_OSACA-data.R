180825619055349f540732a6778a7e144c65e11b
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-17 00:51:01 +0000

    added sample data set 5, based on 296 profiles from the work of F. Carre in France
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@360 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- test_suite/process_OSACA-data.R
+++ test_suite/process_OSACA-data.R
@@ -4,27 +4,27 @@ library(aqp)
 x <- read.csv('OSACA-example_soil_data.csv')
 
 # assume horizons are measured from 0
-x.new <- ddply(x, .(Soil), .fun=function(i) {data.frame(i, top=c(0, i$Depth.D.[-length(i$Depth.D.)]), bottom=i$Depth.D.)})
+x.new <- ddply(x, .(soil), .fun=function(i) {data.frame(i, top=c(0, i$depth.[-length(i$depth)]), bottom=i$depth)})
 
 # fix depths
 x.new$top <- as.integer(x.new$top)
 x.new$bottom <- as.integer(x.new$bottom)
 
-# temp id
-x.new$id <- as.numeric(x.new$Soil)
-
-# give it a try
-d <- profile_compare(x.new, vars=c('Sand.D.','MG.D.','EC.D.'), replace_na=TRUE, max_d=100, k=0, add_soil_flag=TRUE)
-
 # figure out column names / meanings\
 # R,G,B appear to be color data
-x.new$soil_color <- rgb(x.new$R25.D., g=x.new$G25.D., b=x.new$B25.D.)
-plot(Depth.D. ~ id, data=x.new, col=x.new$soil_color, pch=15, ylim=c(400, 0))
+x.new$soil_color <- rgb(x.new$R25, g=x.new$G25, b=x.new$B25)
+plot(depth ~ soil, data=x.new, col=x.new$soil_color, pch=15, ylim=c(400, 0))
 
-xx <- x.new[1:200, ]
-depths(xx) <- id ~ top + bottom
-profile_plot(xx, name='hor', max.depth=250)
+# SPC
+depths(x.new) <- soil ~ top + bottom
+
+# give it a try: seems reasonable
+d <- profile_compare(x.new, vars=c('sand','Mg','EC'), replace_na=TRUE, max_d=100, k=0, add_soil_flag=TRUE)
 
 # save to .Rdata
-# document
-# package
+sp5 <- x.new
+sp5@horizons$depth <- NULL
+sp5@horizons$hor <- NULL
+sp5@horizons$soil <- as.character(sp5@horizons$soil)
+
+save(sp5, file='sp5.rda')
