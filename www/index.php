876ac3ce9a938c037cbe77cc3dde1f0383a8690c
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-26 23:28:32 +0000

    adding WWW content
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@546 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -61,6 +61,8 @@ R-Forge (http://aqp.r-forge.r-project.org).
 </p>
 
 
+<img src="hz_distinctness_demo.png" border="0">
+
 <img src="hz_vs_site-and-hz_dend.png" border="0">
 
 <img src="sanguinetti-pedons.png" border="0">
