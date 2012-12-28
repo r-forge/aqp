5953d197888991362ad7a1d15f8951fb5fc55ab3
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2012-12-28 22:37:38 +0000

    10x speed improvement in slab after switching to aggregate(). there are still a couple of features that are broken. see slab.R for remaining TODO items.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@630 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- test_suite/slab-profiling.R
+++ test_suite/slab-profiling.R
@@ -1,26 +1,54 @@
 library(aqp)
+library(profr)
 library(plyr)
 library(lattice)
 
 # generate some fake soils data
-d <- ldply(1:1000, random_profile)
+d <- ldply(1:100, random_profile)
 depths(d) <- id ~ top + bottom
 
 # create several groups of different sizes
-d$g0 <- rep(1:100, each=10) # 10 / group
-d$g1 <- rep(1:20, each=50) # 50 / group
-d$g2 <- rep(1:10, each=100) # 100 / group
-d$g3 <- rep(1:5, each=200) # 200 / group
+d$g1 <- rep(1:20, each=5) # 5 / group
+d$g2 <- rep(1:10, each=10) # 10 / group
+d$g3 <- rep(1:5, each=20) # 20 / group
+
+# profile slice method: pretty quick, scales linearly
+system.time(s <- slice(d, 1:100 ~ p1 + p2 + p3))
+p <- profr(s <- slice(d, 1:100 ~ p1 + p2 + p3))
+plot(p, cex=0.5, minlabel=0.01)
+
+# no groups: 3.2 seconds !
+system.time(s <- slab(d, ~ p1 + p2 + p3))
+p <- profr(s <- slab(d, ~ p1 + p2 + p3))
+plot(p, cex=0.5, minlabel=0.01)
+
+# minimal grouping: 10x slower ~ 45 seconds using idata.frame, 42 second without it
+# ddply/ldply are the slowest components
+# slice is only a small fraction of the total time (1.3 seconds out of 45+ seconds for slab)
+# 4 seconds with aggregate
+system.time(s <- slab(d, g3 ~ p1 + p2 + p3))
+p <- profr(s <- slab(d, g3 ~ p1 + p2 + p3))
+plot(p, cex=0.5, minlabel=0.01)
+
+
+p.i <- ddply(p, .(f), .fun=summarise, total_time=sum(time))
+p.i <- p.i[order(p.i$total_time, decreasing=TRUE), ]
+p.i$prop <- p.i$total_time / p.i$total_time[1]
+p.h <- head(p.i[-1, ], 10)
+	
+dotplot(f ~ total_time, data=p.h)
+
+
+
 
 # aggregate across varying group sizes
 s <- system.time(slab(d, ~ p1 + p2 + p3))[3]
-s0 <- system.time(slab(d, g0 ~ p1 + p2 + p3))[3]
 s1 <- system.time(slab(d, g1 ~ p1 + p2 + p3))[3]
 s2 <- system.time(slab(d, g2 ~ p1 + p2 + p3))[3]
 s3 <- system.time(slab(d, g3 ~ p1 + p2 + p3))[3]
 
 # combine results into data.frame
-tt <- data.frame(profiles.per.group=factor(c(10, 50, 100, 200, 1000)), time=c(s0, s1, s2, s3, s))
+tt <- data.frame(profiles.per.group=factor(c(5, 10, 20, 100)), time=c(s1, s2, s3, s))
 
 # viz results
 xyplot(time ~ profiles.per.group, data=tt, type='b', pch=15, cex=2, col='black')
