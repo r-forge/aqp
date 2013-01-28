0ee2cb08401be5d9863ad0fbe1db4fac34e1e9c4
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2013-01-28 23:15:09 +0000

    adding sharpshootR package www content
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@652 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- docs/knit_man_pages.R
+++ docs/knit_man_pages.R
@@ -1,6 +1,6 @@
 library(knitr)
 
-opts_chunk$set(message=FALSE, warning=FALSE, background='#F7F7F7', dpi=100, fig.align='center', fig.height=6, fig.width=6, dev='png', tidy=FALSE)
+opts_chunk$set(message=FALSE, warning=FALSE, background='#F7F7F7', dpi=100, fig.align='center', fig.height=6, fig.width=6, dev='CairoPNG', tidy=FALSE)
 options(width=100, stringsAsFactors=FALSE)
 
 setwd('C:/home/Dylan.Beaudette/working_copies/aqp/www/aqp-html-manual/')
@@ -13,3 +13,6 @@ knit_rd('soilDB')
 unlink('figure', recursive=TRUE)
 
 
+setwd('C:/home/Dylan.Beaudette/working_copies/aqp/www/sharpshootR-html-manual/')
+knit_rd('sharpshootR')
+unlink('figure', recursive=TRUE)
