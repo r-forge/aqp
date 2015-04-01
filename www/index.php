8b84b67be6dbbdbe98894b5358281d2f40e45fc7
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-01 16:27:25 +0000

    added 2015 GHL webinar presentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1033 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -46,6 +46,7 @@ echo $contents; } ?>
 
 <p><b>Presentations</b>:
 <ol>
+<li><a href="https://r-forge.r-project.org/scm/viewvc.php/*checkout*/docs/presentations/ghl-aggregation.html?root=aqp">Soil Data Aggregation</a></li>
 <li><a href="https://r-forge.r-project.org/scm/viewvc.php/*checkout*/docs/presentations/aqp-esd-dsm.html?root=aqp">AQP, DSM, and ESD</a></li>
 <li><a href="https://r-forge.r-project.org/scm/viewvc.php/*checkout*/docs/presentations/AQP-num_soil_classification.pdf?root=aqp">Numerical Classification with AQP</a></li>
 <li><a href="https://r-forge.r-project.org/scm/viewvc.php/*checkout*/docs/presentations/AQP-soilDB-beaudette.pdf?root=aqp">AQP and SoilDB Demo</a></li>
