613b7c787c3c9ee02f9753c20d4b8151914c5c50
roudierp (Dr. ) <pierre.roudier@gmail.com>
2015-06-08 23:29:06 +0000

    Implemented the `[i,j` method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1089 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/aqp2/R/SoilProfileCollection-overloads.R
+++ sandbox/aqp2/R/SoilProfileCollection-overloads.R
@@ -114,4 +114,86 @@ setMethod(
   }
 )
 
+## matrix / DF style access: only to horizon data
+##
+## i = profile index
+## j = horizon / slice index
+##
+setMethod("[", "SoilProfile",
+          function(x, i, j, ...) {
+            # In this case i is always one
+            # Now subsetting horizon data using j
+            
+            if(!missing(j)) {
+              
+              j <- as.integer(j)
+              
+              if(any(is.na(j))) {
+                stop('NA not permitted in horizon/slice index', call.=FALSE)
+              }
+              
+            } else {
+              j <- 1:nrow(x@horizons)
+            }
+            
+            # Subset horizons in the profiles
+            SoilProfile(id = x@id,
+                  depths = x@depths[j,, drop = FALSE],
+                  depth_units = depth_units(x),
+                  sp = x@sp,
+                  horizons = x@horizons[j,, drop = FALSE],
+                  site = x@site
+            )
+          }
+)
 
+setMethod("[", "SoilProfileCollection",
+        function(x, i, j, ...) {
+            
+          # check for missing i and j
+          if(missing(i) & missing(j)) {
+            stop('must provide either a profile index or horizon/slice index, or both', call.=FALSE)
+          }
+          
+          # convert to integer
+          if(!missing(i)) {
+            
+            if(any(is.na(i))) {
+              stop('NA not permitted in profile index', call.=FALSE)
+            }
+              
+            # convert logical to integer per standard vector/list
+            # indexing rules (thanks Jos? Padarian for the suggestion!)
+            if(is.logical(i)) {
+              i <- (1:length(x))[i]
+            }
+            
+            i <- as.integer(i)
+            
+          } else { # if no index is provided, the user wants all profiles
+            i <- 1:length(x)
+          }
+          
+          # Subset profiles 
+          spc <- profiles(x)[i]
+          
+          # Now subsetting horizon data using j
+          if(!missing(j)) {
+            
+            j <- as.integer(j)
+            
+            if(any(is.na(j))) {
+              stop('NA not permitted in horizon/slice index', call.=FALSE)
+            }
+            
+          }
+          
+          # Subset horizons in the profiles
+          lspc <- lapply(spc, function(s) s[, j])
+          
+          SoilProfileCollection(profiles = lspc)
+          
+        }
+      )
+
+            
