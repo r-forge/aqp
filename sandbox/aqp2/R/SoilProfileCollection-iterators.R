113ae63d7709954fd86bffd0684528f2e4d45992
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-08 19:28:52 +0000

    reimplemented profileApply for the new classes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1076 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/aqp2/R/SoilProfileCollection-iterators.R
+++ sandbox/aqp2/R/SoilProfileCollection-iterators.R
@@ -1,35 +1,18 @@
 # # analog to apply():
-# # operates by profile, returns vector of length(object) or nrow(object)
-# if (!isGeneric("profileApply"))
-#  	setGeneric("profileApply", function(object, FUN, simplify=TRUE, ...) standardGeneric("profileApply"))
-# 
-# setMethod(f='profileApply', signature='SoilProfileCollection',
-# 	function(object, FUN, simplify=TRUE, ...) {
-# 						
-# 		# get profile IDs
-# 		pIDs <- profile_id(object)
-# 					
-# 		# init empty list
-# 		l <- list()
-# 						
-# 		# iterate over SPC, spliting into a list of SPC_i ... SPC_n
-# 		for(i in seq_along(pIDs)) {
-# 			pID <- pIDs[i]
-# 			l[[pID]] <- do.call(FUN, list(object[i, ], ...))
-# 		}
-# 		
-# 		# optionally simplify
-# 		if(simplify) {
-# 			res <- unlist(l)
-# 			return(res)
-# 		}
-# 		else
-# 			return(l)
-# 	}
-# )
+# operates by profile, returns vector of length(object) or nrow(object)
+if (!isGeneric("profileApply"))
+ 	setGeneric("profileApply", function(object, FUN, simplify=TRUE, ...) standardGeneric("profileApply"))
 
-
-
-
-
-
+setMethod(f = 'profileApply', signature = 'SPC',
+	function(object, FUN, simplify = TRUE, ...) {
+	  
+	  res <- lapply(profiles(object), FUN = FUN, ...)
+						
+		# optionally simplify
+		if(simplify) {
+			res <- unlist(res)
+		}
+		
+		return(res)
+  }
+)
