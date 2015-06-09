3f50cef8b3170682ec31a55baec7d5ba86475573
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-09 16:12:44 +0000

    I was responsible for that regression
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1096 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/aqp2/R/SoilProfileCollection-methods.R
+++ sandbox/aqp2/R/SoilProfileCollection-methods.R
@@ -218,7 +218,11 @@ setMethod("profiles", "SoilProfileCollection",
     if (is.null(i)) {
       res <- object@profiles
     } else {
-      res <- object@profiles[i]
+      if (length(i) > 1) {
+        res <- object@profiles[i]
+      } else {
+        res <- object@profiles[[i]]
+      }
     }
     res
   }
