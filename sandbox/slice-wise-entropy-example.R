e2694965f814b7b7c8bfebd9effd136241313fb7
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-22 21:40:45 +0000

    some fixing of the no-information idea
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/slice-wise-entropy-example.R
+++ sandbox/slice-wise-entropy-example.R
@@ -51,6 +51,9 @@ f.qcd <- function(v) {
   return(res)
 }
 
+## this isn't possible as slab functions can only "see" the current slab
+# QCD of z-scores ?
+
 mean.and.sd <- function(values) {
   m <- mean(values, na.rm=TRUE)
   s <- sd(values, na.rm=TRUE)
@@ -61,70 +64,36 @@ mean.and.sd <- function(values) {
 }
 
 
-# compute some "information" metrics
-a <- slab(sp3,  ~ clay + A + cec + ph, slab.fun=mean.and.sd, slab.structure=0:100)
-a.1 <- slab(sp3,  ~ clay + A + cec + ph, slab.fun=f.entropy, slab.structure=0:100)
-a.2 <- slab(sp3, ~ clay + A + cec + ph, slab.fun=f.sig.to.noise, slab.structure=0:100)
-a.3 <- slab(sp3, ~ clay + A + cec + ph, slab.fun=f.qcd, slab.structure=0:100)
+
+## does this make sense?
+# simulate a no-information, baseline QCD, from all original, sliced values
+no.information.qcd <- function(i, n.slices=101){
+  # "i" is a chunk of data.frame
+  x <- i$value
+  
+  # remove NA
+  not.NA.idx <- which(!is.na(x))
+  x <- x[not.NA.idx]
+  
+  # remove O's
+  non.zero.idx <- which(x != 0)
+  x <- x[non.zero.idx]
+  
+  # this is the mean QCD over then entire set of property 'x'
+  qcd <- IQR(x) / median(x)
+  
+  # this is the sum of mean QCD over the number of depth slices
+  qcd.sum <- qcd * n.slices
+  
+  d <- data.frame(mean.ni.qcd=qcd, sum.ni.qcd=qcd.sum)
+  return(d)
+}
 
 
-## TODO: WTF am I trying to accomplish here?
-# # simulate a no-information comparison
-# no.information.qcd <- function(i){
-#   # "i" is a chunk of data.frame
-#   x <- i$value
-#   w <- i$contributing_fraction
-#   
-#   # remove NA
-#   not.NA.idx <- which(!is.na(x))
-#   x <- x[not.NA.idx]
-#   w <- w[not.NA.idx]
-#   
-#   # remove O's
-#   non.zero.idx <- which(x != 0)
-#   x <- x[non.zero.idx]
-#   w <- w[non.zero.idx]
-#   
-#   # re-sampled sum(QCD) from 10x original number of slices
-#   n <- length(x)
-#   s <- replicate(n, sample(x, size=n*10, prob = w, replace = TRUE))
-#   base.qcd <- apply(s, 2, function(i) IQR(i) / median(i))
-#   
-#   d <- data.frame(value=base.qcd, lower=NA, upper=NA)
-#   return(d)
-# }
-# 
-# s <- slice(sp3, 0:100 ~ clay + A + cec + ph, just.the.data = TRUE)
-# s.long <- melt(s, id.vars = c('top', 'bottom'), measure.vars = c('clay', 'A', 'cec', 'ph'))
-# ddply(s.long, c('top', 'bottom', 'variable'), no.information.qcd)
 
-
-# combine
-g <- make.groups(summary=a, entropy=a.1, sig.to.noise=a.2, qcd=a.3)
-g$which <- factor(g$which, labels=c('Mean +/- 1SD', 'psuedo-Entropy', 'Signal : Noise', 'QCD'))
-
-p <- xyplot(
-  top ~ value | which + variable, data=g,
-  lower=g$lower, upper=g$upper, sync.colors=TRUE, alpha=0.5,
-  cf=g$contributing_fraction,
-  ylab='Depth (cm)',
-  xlab='',
-  ylim=c(100,-5), layout=c(5,3), scales=list(x=list(relation='free')),
-  par.settings=list(superpose.line=list(lwd=2, col=c('RoyalBlue', 'Orange2'))),
-  panel=panel.depth_function, 
-  prepanel=prepanel.depth_function,
-  auto.key=list(columns=2, lines=TRUE, points=FALSE)
-)
-
-useOuterStrips(p, strip=strip.custom(bg=grey(0.85)), strip.left = strip.custom(horizontal=FALSE, bg=grey(0.85)))
-
-
-## investigate weighted mean QCD, weighted by contributing fraction
-
+## weighted mean QCD, weighted by contributing fraction
 ## must remove 0's and NA before computing weighted mean QCD
 ## when n=1, IQR = 0 => QCD = 0
-##
-## compare wt.mean QCD to empirical baseline
 wtd.mean.qcd <- function(i){
   # "i" is a chunk of data.frame
   x <- i$value
@@ -141,16 +110,19 @@ wtd.mean.qcd <- function(i){
   w <- w[non.zero.idx]
   res <- wtd.mean(x, weights=w, na.rm=TRUE)
   
-  # normalize to some kind of baseline
-  n <- length(x)
-  r <- sample(x, size=n * 100, prob = w, replace = TRUE)
-  base.qcd <- IQR(r) / median(r)
+  #   ## this doesn't make much sense...
+  #   # normalize to some kind of baseline
+  #   n <- length(x)
+  #   r <- sample(x, size=n * 100, prob = w, replace = TRUE)
+  #   base.qcd <- IQR(r) / median(r)
   
-  d <- data.frame(qcd=res, qcd.norm=res/base.qcd)
+  d <- data.frame(wt.mean.qcd=res)
   return(d)
 }
 
-## weighted sum QCD and comparison to empirical baseline
+## weighted sum QCD
+## must remove 0's and NA before computing weighted mean QCD
+## when n=1, IQR = 0 => QCD = 0
 wtd.sum.qcd <- function(i){
   # "i" is a chunk of data.frame
   x <- i$value
@@ -169,19 +141,60 @@ wtd.sum.qcd <- function(i){
   # compute weighted sum
   res <- sum(x * w)
   
-  # normalize to some kind of baseline,
-  # re-sampled sum(QCD) from 10x original number of slices
-  n <- length(x)
-  s <- replicate(n, sample(x, size=n*10, prob = w, replace = TRUE))
-  base.qcd <- sum(apply(s, 2, function(i) IQR(i) / median(i)))
+  #   ## this doesn't make much sense...
+  #   # normalize to some kind of baseline,
+  #   # re-sampled sum(QCD) from 10x original number of slices
+  #   n <- length(x)
+  #   s <- replicate(n, sample(x, size=n*10, prob = w, replace = TRUE))
+  #   base.qcd <- sum(apply(s, 2, function(i) IQR(i) / median(i)))
   
-  d <- data.frame(qcd=res, qcd.norm=res/base.qcd)
+  d <- data.frame(wt.sum.qcd=res)
   return(d)
 }
 
-## how is this useful?
-## TODO: compare between soils
-ddply(a.3, 'variable', .fun=wtd.mean.qcd)
-ddply(a.3, 'variable', .fun=wtd.sum.qcd)
+
+# compute some "information" metrics
+a <- slab(sp3,  ~ clay + A + cec + ph, slab.fun=mean.and.sd, slab.structure=0:100)
+a.1 <- slab(sp3,  ~ clay + A + cec + ph, slab.fun=f.entropy, slab.structure=0:100)
+a.2 <- slab(sp3, ~ clay + A + cec + ph, slab.fun=f.sig.to.noise, slab.structure=0:100)
+a.3 <- slab(sp3, ~ clay + A + cec + ph, slab.fun=f.qcd, slab.structure=0:100)
+
+# combine
+g <- make.groups(summary=a, entropy=a.1, sig.to.noise=a.2, qcd=a.3)
+g$which <- factor(g$which, labels=c('Mean +/- 1SD', 'psuedo-Entropy', 'Signal : Noise', 'QCD'))
+
+p <- xyplot(
+  top ~ value | which + variable, data=g,
+  lower=g$lower, upper=g$upper, sync.colors=TRUE, alpha=0.5,
+  cf=g$contributing_fraction,
+  ylab='Depth (cm)',
+  xlab='',
+  ylim=c(100,-5), layout=c(5,3), scales=list(x=list(relation='free')),
+  par.settings=list(superpose.line=list(lwd=2, col=c('RoyalBlue', 'Orange2'))),
+  panel=panel.depth_function, 
+  prepanel=prepanel.depth_function,
+  auto.key=list(columns=2, lines=TRUE, points=FALSE)
+)
+
+## not quite right, as x-axis shouldn't float...
+useOuterStrips(p, strip=strip.custom(bg=grey(0.85)), strip.left = strip.custom(horizontal=FALSE, bg=grey(0.85)))
+
+
+## a "no-information QCD" must be computed from the raw data, by depth-slice
+s <- slice(sp3, 0:100 ~ clay + A + cec + ph, just.the.data = TRUE)
+s.long <- melt(s, id.vars = c('top', 'bottom'), measure.vars = c('clay', 'A', 'cec', 'ph'))
+qcd.ni <- ddply(s.long, c('variable'), no.information.qcd)
+
+
+## compute weighted mean and weighted sum QCD by variable
+## note that these must be standardized by slice-wise "no-information" QCD
+wm.qcd <- ddply(a.3, 'variable', .fun=wtd.mean.qcd)
+ws.qcd <- ddply(a.3, 'variable', .fun=wtd.sum.qcd)
+
+### does this make sense?
+# join QCD summaries to "no-information" baseline
+ss <- join(join(wm.qcd, ws.qcd), qcd.ni)
+transform(ss, mean.qcd=wt.mean.qcd / mean.ni.qcd, sum.qcd=wt.sum.qcd / sum.ni.qcd)
+
 
 
