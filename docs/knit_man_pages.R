dde4e0ff69ee50106234b08781c642680113291d
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2015-11-20 20:04:38 +0000

    final update for a while to SDA query tutorial
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1180 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- docs/knit_man_pages.R
+++ docs/knit_man_pages.R
@@ -1,6 +1,6 @@
 library(knitr)
 
-opts_chunk$set(message=FALSE, warning=FALSE, background='#F7F7F7', dpi=100, fig.align='center', fig.height=6, fig.width=6, dev='CairoPNG', tidy=FALSE)
+opts_chunk$set(message=FALSE, warning=FALSE, background='#F7F7F7', dpi=100, fig.align='center', fig.height=6, fig.width=6, dev='png', tidy=FALSE)
 options(width=100, stringsAsFactors=FALSE)
 
 setwd('C:/home/Dylan.Beaudette/working_copies/aqp/www/aqp-html-manual/')
