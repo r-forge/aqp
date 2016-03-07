dcbe3583424f017852386d5c0af8ed1573bb7f46
dylan (Dylan Beaudette) <dylan.beaudette@gmail.com>
2016-03-07 19:59:27 +0000

    fixes to munsell2rgb doc updates and better neutral colors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aqp@1284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- utils/add-neutral-hue.R
+++ utils/add-neutral-hue.R
@@ -5,7 +5,20 @@ data(munsell)
 m <- munsell
 
 # add records for neutral hues
-N <- expand.grid(hue='N', value=1:10, chroma=0, r=0.2, g=0.2, b=0.2)
+hue <- 'N'
+value <- c(2, 3, 4, 5, 6, 7, 8)
+chroma <- 0
+
+N <- data.frame(hue=hue, value=value, chroma=chroma, stringsAsFactors = FALSE)
+
+# estimate colors via grey()
+greys <- as.data.frame(t(col2rgb(grey(value/10))) / 255)
+names(greys) <- c('r', 'g', 'b')
+
+N <- cbind(N, greys)
+
+# check
+with(N, plot(value ~ chroma, col=rgb(r, g, b), pch=15, cex=5, axes=FALSE))
 
 # add at bottom
 munsell <- rbind(m, N)
